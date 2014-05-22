c080a45369209ea41480eeec87a084405bc73431
Okal Otieno <okal@justokal.com>
2014-05-22 09:25:48 +0300

    Make script accept filename argument

--- cheza.py
+++ cheza.py
@@ -3,12 +3,14 @@ import sys
 
 from mutagen.easyid3 import EasyID3
 from pydub import AudioSegment
+from clint import textui
+from time import sleep
 
 
 class Song:
     def __init__(self, mp3_file):
         self.mp3_file = mp3_file
-        self.duration = AudioSegment.from_mp3(mp3_file).duration_seconds
+        self.duration = int(AudioSegment.from_mp3(mp3_file).duration_seconds)
         self.pygame = pygame
         self.paused = None
         self.pygame.init()
@@ -35,5 +37,24 @@ class Song:
 
     def stop(self):
         self.pygame.mixer.stop()
-        print "Kwaheri :-("
         sys.exit(0)
+
+
+if __name__ == '__main__':
+    song_file = sys.argv[1]
+    song = Song(song_file)
+    progress_bar = textui.progress.bar(range(song.duration))
+    try:
+        print textui.colored.blue("Cheza Media Player")
+        print textui.colored.blue("Karibu")
+        print textui.colored.green(song.get_metadata())
+        progress_bar.send(None)
+        song.play()
+        while song.pygame.mixer.music.get_busy():
+            sleep(1)
+            progress_bar.send(int(song.pygame.mixer.music.get_pos())/1000)
+    except KeyboardInterrupt:
+        progress_bar.close()
+        print ''
+        print textui.colored.red("Kwaheri :-(")
+        song.stop()
