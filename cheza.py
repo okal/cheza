ca58bc404b07ea3154128d84367e4c2a1169ceaf
Okal Otieno <okal@justokal.com>
2014-05-22 08:10:52 +0300

    Get metadata from mp3 file

--- cheza.py
+++ cheza.py
@@ -3,7 +3,14 @@ from mutagen.easyid3 import EasyID3
 
 class Player:
     def __init__(self, mp3_file):
+        self.mp3_file = mp3_file
         pass
+
+    def get_metadata(self):
+        items = EasyID3(self.mp3_file).items()
+        meta = '\n'.join(
+            map(lambda kv: ''.join([kv[0], ": ", kv[1][0]]), items))
+        return meta
 
     def play(self):
         pass
