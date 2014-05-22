783936ce4b231d8f4b60726cc2ddb94a944290e8
Okal Otieno <okal@justokal.com>
2014-05-22 08:27:19 +0300

    Add song_duration property to metadata

--- cheza.py
+++ cheza.py
@@ -1,15 +1,18 @@
 from mutagen.easyid3 import EasyID3
+from pydub import AudioSegment
 
 
 class Player:
     def __init__(self, mp3_file):
         self.mp3_file = mp3_file
-        pass
+        self.song_duration = AudioSegment.from_mp3(mp3_file).duration_seconds
 
     def get_metadata(self):
         items = EasyID3(self.mp3_file).items()
+        items.append(['length', [str(self.song_duration)]])
         meta = '\n'.join(
-            map(lambda kv: ''.join([kv[0], ": ", kv[1][0]]), items))
+            map(lambda kv: ''.join([kv[0].capitalize(), ": ", kv[1][0]]),
+                items))
         return meta
 
     def play(self):
