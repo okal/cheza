e3aff2288b8a36a4dfb08aa9296f224bae6587bb
Okal Otieno <okal@justokal.com>
2014-05-22 08:49:19 +0300

    Rename song_duration property to duration

--- cheza.py
+++ cheza.py
@@ -7,11 +7,11 @@ from pydub import AudioSegment
 class Song:
     def __init__(self, mp3_file):
         self.mp3_file = mp3_file
-        self.song_duration = AudioSegment.from_mp3(mp3_file).duration_seconds
+        self.duration = AudioSegment.from_mp3(mp3_file).duration_seconds
 
     def get_metadata(self):
         items = EasyID3(self.mp3_file).items()
-        items.append(['length', [str(self.song_duration)]])
+        items.append(['length', [str(self.duration)]])
         meta = '\n'.join(
             map(lambda kv: ''.join([kv[0].capitalize(), ": ", kv[1][0]]),
                 items))
